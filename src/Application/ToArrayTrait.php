44b0880869df501902eb50f90fb7564ec657621c
Andrew Wheal <andreww@propcom.co.uk>
2018-02-08 17:49:39 +0000

    Stop removing empty properties

--- src/Application/ToArrayTrait.php
+++ src/Application/ToArrayTrait.php
@@ -10,9 +10,7 @@ trait ToArrayTrait
 		$array = get_object_vars($this);
 
 		foreach ($array as $key => $value) {
-			if (is_null($value) or $value === []) {
-				unset($array[$key]);
-			} elseif (is_array($value)) {
+			if (is_array($value)) {
 				foreach ($value as $subkey => $subvalue) {
 					$array[$key][$subkey] = $subvalue->toArray();
 				}
